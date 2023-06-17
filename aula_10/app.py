f8c7a23612b65f2d14c1ca1fe6247d2d10dee5a0
Flavio Souza <flavioluiz.ssouza@gmail.com>
2023-06-16 21:52:40 -0300

    Aula 10 - fix error handler

--- aula_10/app.py
+++ aula_10/app.py
@@ -1,3 +1,5 @@
+
+from werkzeug.exceptions import HTTPException
 from flask import Flask, jsonify
 from flask_restx import Api
 from endpoints.rating_endpoint import ns as ns_rating
@@ -15,25 +17,9 @@ api = Api(
 api.add_namespace(ns_rating)
 api.add_namespace(ns_movie)
 
-@app.errorhandler(400)
-def _bad_request(e):
-    return jsonify(error=str(e)), 400
-
-@app.errorhandler(403)
-def _forbidden(e):
-    return jsonify(error=str(e)), 403
-
-@app.errorhandler(404)
-def _not_found(e):
-    return jsonify(error=str(e)), 404
-
-@app.errorhandler(409)
-def _conflict(e):
-    return jsonify(error=str(e)), 409
-
-@app.errorhandler(413)
-def _payload_too_large(e):
-    return jsonify(error=str(e)), 413
+@app.errorhandler(HTTPException)
+def handle_exception(e):
+    return jsonify(error=str(e)), e.code
 
 if __name__ == '__main__':
     app.run(debug=True)
